94a8677378e9a615aa649d33e83f99da8373551c
Queren Hapuque <queren.reis@ufpi.edu.br>
2026-06-15 22:04:39 -0300

    início- Definição do menu .

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+print("======================================")
+print("SISTEMA: CALCULADORA E CAIXA ")
+print("======================================")
+print("1-Resolver quatro operações básicas !")
+print("2-fluxo de caixa (cálculo de troco)")
+print("3-multiplicação de matriz (2x2)")
+print("escolha uma opção: ")
+
+opcao=input("Digite o número da opção desejada: ")
